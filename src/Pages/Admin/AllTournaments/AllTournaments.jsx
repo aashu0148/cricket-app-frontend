7cc944154247050874208c95b16d3963ba6f1940
Natalya732 <nikkupandey0602@gmail.com>
2024-09-22 16:25:23 +0530

    chnages doing in admin alltournament page

--- src/Pages/Admin/AllTournaments/AllTournaments.jsx
+++ src/Pages/Admin/AllTournaments/AllTournaments.jsx
@@ -5,13 +5,72 @@ import Button from "@/Components/Button/Button";
 import { getAllTournaments } from "@/apis/tournament";
 
 import styles from "./AllTournaments.module.scss";
-import { espnOrigin } from "@/utils/constants";
-import { getMatchScoreCardUrl } from "@/utils/util";
+
 import PageLoader from "@/Components/PageLoader/PageLoader";
+import { CheckCircle } from "react-feather";
+import { getMatchScoreCardUrl } from "@/utils/util";
 
 export default function AllTournaments() {
   const [allTournaments, setAllTournaments] = useState([]);
   const [loading, setLoading] = useState(true);
+
+  const Card = ({ data, tournament }) => {
+    const completed = tournament.completedMatches.filter(
+      (item) => item.objectId === data.objectId
+    );
+
+    const matchName = toCapitilizeString(data?.slug);
+    const matchLink = getMatchScoreCardUrl({
+      tournamentSlug: tournament.slug,
+      tournamentObjectId: tournament.objectId,
+      matchSlug: data.slug,
+      matchObjectId: data.objectId,
+    });
+
+    return (
+      <div className={styles.card}>
+        <div className={styles.card_header}>
+          <h2>{matchName}</h2>
+          {completed.length ? (
+            <span>
+              <CheckCircle size={"18px"} color="green" />
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
+        <div className={styles.card_content}>
+          <a href={matchLink}>Click Here</a>
+          {completed.length ? (
+            <div className={styles.card_status}>
+              <p>Status</p>
+              <h4>{data.statusText}</h4>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    );
+  };
+
+  const PlayerCard = ({ player }) => {
+    return (
+      <div className={styles.playerCard}>
+        <div className={styles.imageContainer}>
+          <img
+            src={player.image}
+            alt={player.name}
+            className={styles.playerImage}
+          />
+        </div>
+        <h3 className={styles.playerName}>{player.name}</h3>
+        <p className={styles.playerCountry}>({player.country})</p>
+      </div>
+    );
+  };
+
+  // ******************************* Functions ****************************
 
   async function fetchTournaments() {
     const res = await getAllTournaments();
@@ -23,7 +82,7 @@ export default function AllTournaments() {
     setAllTournaments(res?.data);
   }
 
-  const toCapitilizeString = (str) => {
+  function toCapitilizeString(str) {
     if (!str) return;
 
     const newString = str.split("-").join(" ");
@@ -31,17 +90,18 @@ export default function AllTournaments() {
       .split(" ")
       .map((item) => item.charAt(0).toUpperCase() + item.slice(1))
       .join(" ");
-  };
+  }
 
   useEffect(() => {
     fetchTournaments();
   }, []);
 
   return loading ? (
-    <PageLoader fullPage/>
+    <PageLoader fullPage />
   ) : (
     <div className={`${styles.container} page-container`}>
       <h1 className={"gradient-text"}>All Tournaments</h1>
+
       {allTournaments?.length &&
         allTournaments?.map((tournament) => (
           <div className={styles.tournamentContainer} key={tournament._id}>
@@ -52,38 +112,32 @@ export default function AllTournaments() {
               </div>
               <Button>Edit</Button>
             </div>
+
+            {/************************************ Match Details ************************/}
             <div className={styles.matchesDetails}>
               <h3>
-                Matches <span>count</span>
+                Match Count <span>{tournament.allMatches.length}</span>
               </h3>
-              {tournament?.allMatches?.map((item) => (
-                <div className={`${styles.matchesDetails_content} row`}>
-                  <div className={`${styles.singleMatchContainer} flex-col-xs`}>
-                    <div>{toCapitilizeString(item?.slug)}</div>
-                    <a
-                      href={getMatchScoreCardUrl({
-                        tournamentSlug: tournament.slug,
-                        tournamentObjectId: tournament.objectId,
-                        matchSlug: item.slug,
-                        matchObjectId: item.objectId,
-                      })}
-                      target="_blank"
-                    >
-                      Click Here
-                    </a>
+              <div className={styles.matchesDetails_content_scroll}>
+                {tournament.allMatches.map((item) => (
+                  <div>
+                    <Card data={item} tournament={tournament} />
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
-            <div className={styles.playerDetails}>
+
+            {/************************************ Player Details ************************/}
+            <div className={styles.matchesDetails}>
               <h3>
-                Players <span>count</span>
+                Players Count <span>{tournament.players.length}</span>
               </h3>
-              <div className={styles.playerDetails_content}>
-                <div className={`${styles.singlePlayerContainer} flex-col-xs`}>
-                  <img alt="imga"></img>
-                  <p>Player Name</p>
-                </div>
+              <div className={styles.player_scrollBar}>
+                {tournament.players.map((item) => (
+                  <div>
+                    <PlayerCard player={item} />
+                  </div>
+                ))}
               </div>
             </div>
           </div>
