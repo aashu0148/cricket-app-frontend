b8bbe0a188ae079f4938136c9b842a2fe59a8b8f
Aashu Sharma <buildforfb@gmail.com>
2024-10-17 09:10:14 +0530

    small change

--- src/Pages/Admin/ScoringSystem/EditScoringSystem/EditScoringSystem.jsx
+++ src/Pages/Admin/ScoringSystem/EditScoringSystem/EditScoringSystem.jsx
@@ -717,7 +717,6 @@ export default function EditScoringSystem() {
     </div>
   );
 
-  console.log(bowlingData, errors);
   const bowlingSection = (
     <div className={styles.section}>
       <h2 className={styles.mainHeading}>Bowling</h2>
